e61838af65bef5604b2c6eb46ed995bc5c59849d
KJM <tdcian@hexlant.com>
2020-01-13 09:12:43 +0900

    gesture recognizer

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 액션 타깃을 통한 제스처 인식기 초기화 및 생성
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapView(gestureRecognizer:)))
+      
+        // 뷰에 제스처 인식기 연결하기
+        self.view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    // 액션 메서드
+    @objc func tapView(gestureRecognizer: UIGestureRecognizer) {
+        print("Tapped")
     }
 
 
